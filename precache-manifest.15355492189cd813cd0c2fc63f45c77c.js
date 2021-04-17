14857d642f91718b429ff208bdb89609cf2fd4e5
hchuzhong <352995096@qq.com>
2021-04-17 19:21:45 +0800

    deploy

---
+++ precache-manifest.15355492189cd813cd0c2fc63f45c77c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a672cdde214d8fd9b7339c86c6f72dbb",
+    "url": "/CountMoney-website/code.png"
+  },
+  {
+    "revision": "270ad4f72a777069cbf0",
+    "url": "/CountMoney-website/css/app.e8047351.css"
+  },
+  {
+    "revision": "08ba3712991ea0788171bd88868c4e8a",
+    "url": "/CountMoney-website/index.html"
+  },
+  {
+    "revision": "270ad4f72a777069cbf0",
+    "url": "/CountMoney-website/js/app.280a89a5.js"
+  },
+  {
+    "revision": "63796ac6813cc50fc636",
+    "url": "/CountMoney-website/js/chunk-vendors.c8145f41.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/CountMoney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/CountMoney-website/robots.txt"
+  }
+]);
